33022413f7f7ae1e8776f8d533657b18371aac69
Muhammad Wasif <mwasif129@gmail.com>
2019-01-14 15:51:56 +0500

    Added ClientResponse DTO

---
+++ Server/src/main/java/com/okta/developer/ims/dto/ClientResponse.java
@@ -0,0 +1,30 @@
+package com.okta.developer.ims.dto;
+
+import java.io.Serializable;
+
+/**
+ * A generic Client Response to be send for each REST service response
+ * it used Message to prompt correct message to user
+ *
+ * @see java.io.Serializable
+ * @see com.okta.developer.ims.dto.Message
+ * @author mwasif
+ *
+ * */
+public class ClientResponse<T> implements Serializable {
+    private Message message;
+    private T data;
+
+    public ClientResponse() {
+    }
+
+    public ClientResponse(Message message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public ClientResponse(Message message) {
+        this.message = message;
+    }
+
+}
